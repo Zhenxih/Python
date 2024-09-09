68fed3edfffd228ac57543bf3584d1e0e7111525
zhenxihe <124065044+Zhenxih@users.noreply.github.com>
2024-09-09 17:51:29 +0200

    Add files via upload

---
+++ Python/Tutorial.py
@@ -0,0 +1,5 @@
+# Pruebas y tutoriales
+print("Hola Mundo")
+print ('Hola Casa')
+
+# Parece que entre funcion y paréntesis puede haber espacios entre ellos y tanto 'comillas simples' como "comillas dobles"
